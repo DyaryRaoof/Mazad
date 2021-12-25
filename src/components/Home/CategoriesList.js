86833dd950d972d21062fc681188c45e928a4f0b
DyaryRaoof <dyary2020@gmail.com>
2021-12-25 22:51:58 +0300

    change categoriesList style to grid

--- src/components/Home/CategoriesList.js
+++ src/components/Home/CategoriesList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './CategoriesList.css';
+import history from '../history';
 
 const generateCategoryListNames = () => {
   const list = [];
@@ -25,7 +27,15 @@ function makeid(length) {
 class CategoriesList extends React.Component {
   renderCateogries(list) {
     return list.map((listItem) => {
-      return <label key={listItem}>{listItem}</label>;
+      return (
+        <div
+          className="category-item"
+          key={listItem}
+          onClick={() => history.push('/job-list')}
+        >
+          {listItem}
+        </div>
+      );
     });
   }
 
@@ -34,28 +44,8 @@ class CategoriesList extends React.Component {
     return (
       <div className="container" style={{ marginTop: '100px' }}>
         <h3 style={{ color: 'green' }}>Job Categories</h3>
-        <div className="row">
-          <div className="col-md-4">
-            {this.renderCateogries(
-              categoryNames.slice(0, categoryNames.length / 3)
-            )}
-          </div>
-          <div className="col-md-4">
-            {this.renderCateogries(
-              categoryNames.slice(
-                categoryNames.length / 3,
-                (categoryNames.length / 3) * 2
-              )
-            )}
-          </div>
-          <div className="col-md-4">
-            {this.renderCateogries(
-              categoryNames.slice(
-                (categoryNames.length / 3) * 2,
-                categoryNames.length
-              )
-            )}
-          </div>
+        <div className="p-2 m-2 categories-grid">
+          {this.renderCateogries(categoryNames)}
         </div>
       </div>
     );
